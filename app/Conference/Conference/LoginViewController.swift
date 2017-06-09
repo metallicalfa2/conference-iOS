3718a100e424b76c9f0fc6e4f704abb2f1c0444b
shubham rathi <shubhamrathi@icloud.com>
2017-06-09 13:16:31 +0530

    login controller added

--- app/Conference/Conference/LoginViewController.swift
+++ app/Conference/Conference/LoginViewController.swift
@@ -9,23 +9,12 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-	@IBOutlet weak var register: UIImageView!
 	
 	@IBOutlet weak var registrationId: UITextField!
-	@IBOutlet weak var connectWithGoogle: UIImageView!
-	@IBOutlet weak var connectWithFb: UIImageView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-		let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
-		register.isUserInteractionEnabled = true
-		register.addGestureRecognizer(tapGestureRecognizer)
     }
-	func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
-	{
-		let vc = TabBarViewController()
-		self.present(vc, animated: true, completion: nil)
-	}
 	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
